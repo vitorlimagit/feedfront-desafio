393797cda1afea78a52862b0eeaca2cc2875e4e3
VitorLima <vitorgl@ciandt.com>
2022-08-17 15:32:26 -0300

    Bloco 1 Implementado

--- src/main/java/com/ciandt/feedfront/App.java
+++ src/main/java/com/ciandt/feedfront/App.java
@@ -6,10 +6,6 @@ import com.ciandt.feedfront.excecoes.ComprimentoInvalidoException;
 import com.ciandt.feedfront.excecoes.EmailInvalidoException;
 import com.ciandt.feedfront.excecoes.EmployeeNaoEncontradoException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws ComprimentoInvalidoException, EmailInvalidoException, ArquivoException, EmployeeNaoEncontradoException {
@@ -29,18 +25,19 @@ public class App
             Employee.salvarEmployee(novoEmployee4);
             Employee.salvarEmployee(novoEmployee5);
             Employee.salvarEmployee(novoEmployee6);
-            System.out.println("Criacao de Employee feito com sucesso!");
+            System.out.println("Employee Criado e Salvo com sucesso!");
         } catch (EmailInvalidoException | ArquivoException ex) {
             System.out.println(ex.getMessage());
         }
         System.out.println("-------------------------------");
         System.out.println("Lista de Employees:");
-        Employee.listarEmployees().forEach(employee -> System.out.println(employee.toString()));
+        Employee.listarEmployees()
+                .forEach(employee -> System.out.println(employee.toString()));
 
         System.out.println("-------------------------------");
 
         try {
-            System.out.println("Busca com Employee com id: " + novoEmployee1.getId());
+            System.out.println("Buscar Employee com id: " + novoEmployee1.getId());
             System.out.println(Employee.buscarEmployee(novoEmployee1.getId()).toString());
 
         } catch (EmployeeNaoEncontradoException | ArquivoException ex) {
@@ -53,7 +50,7 @@ public class App
             novoEmployee1.setSobrenome("da Silva");
 
             Employee atualizaEmployee = Employee.atualizarEmployee(novoEmployee1);
-            System.out.println("Employee Atualizado: "+ atualizaEmployee.toString());
+            System.out.println("Employee Atualizado: " + atualizaEmployee.toString());
         } catch (EmailInvalidoException | ArquivoException ex) {
             System.out.println(ex.getMessage());
         }
--- src/main/java/com/ciandt/feedfront/employee/Employee.java
+++ src/main/java/com/ciandt/feedfront/employee/Employee.java
@@ -5,62 +5,188 @@ import com.ciandt.feedfront.excecoes.ComprimentoInvalidoException;
 import com.ciandt.feedfront.excecoes.EmailInvalidoException;
 import com.ciandt.feedfront.excecoes.EmployeeNaoEncontradoException;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class Employee {
+public class Employee implements Serializable {
     private String id;
     private String nome;
     private String sobrenome;
     private String email;
-    private String arquivo = "arquivo.extensao"; //TODO: alterar de acordo com a sua implementação
+    private String arquivo = "arquivo.extensao";
+    private static final String repositorioPath = "src/main/resources/data/employee/";
+
+    private static final long serialVersionID = 1;
 
     public Employee(String nome, String sobrenome, String email) throws ComprimentoInvalidoException {
         this.id = UUID.randomUUID().toString();
+        this.arquivo = repositorioPath + getId() + ".byte";
+
+        setNome(nome);
+        setEmail(email);
+        setSobrenome(sobrenome);
     }
 
+    private static ObjectOutputStream getOutputStream(String arquivo) throws IOException {
+        return new ObjectOutputStream(new FileOutputStream(arquivo));
+    }
+
+    private static ObjectInputStream getInputStream(String arquivo) throws IOException {
+        return new ObjectInputStream(new FileInputStream(arquivo));
+    }
+
+
     public static Employee salvarEmployee(Employee employee) throws ArquivoException, EmailInvalidoException {
-        return null;
+        ObjectOutputStream outputStream = null;
+
+        try {
+            List<Employee> employees = listarEmployees();
+
+            boolean emailExistente = false;
+            for (Employee employeeSalvo: employees) {
+                if (!employeeSalvo.getId().equals(employee.getId()) && employeeSalvo.getEmail().equals(employee.getEmail())) {
+                    emailExistente = true;
+                    break;
+                }
+            }
+
+            if (emailExistente) {
+                throw new EmailInvalidoException("E-mail ja cadastrado no repositorio");
+            }
+
+            outputStream = getOutputStream(employee.arquivo);
+            outputStream.writeObject(employee);
+
+            outputStream.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            throw new ArquivoException("");
+        }
+
+        return employee;
     }
 
     public static Employee atualizarEmployee(Employee employee) throws ArquivoException, EmailInvalidoException, EmployeeNaoEncontradoException {
-        return null;
+        buscarEmployee(employee.getId());
+
+        return salvarEmployee(employee);
     }
 
     public static List<Employee> listarEmployees() throws ArquivoException {
-        return null;
+        List<Employee> employees = new ArrayList<>();
+
+        try {
+            Stream<Path> paths = Files.walk(Paths.get(repositorioPath));
+
+            List<String> files = paths
+                    .map(p -> p.getFileName().toString())
+                    .filter(p -> p.endsWith(".byte"))
+                    .map(p -> p.replace(".byte", ""))
+                    .collect(Collectors.toList());
+
+            for (String file: files) {
+                try {
+                    employees.add(buscarEmployee(file));
+                } catch (EmployeeNaoEncontradoException e) {
+                    // Exception silenciada porque sei que não chegará aqui
+                }
+            }
+
+            paths.close();
+        } catch (IOException e) {
+            throw new ArquivoException("erro ao processar arquivos");
+        }
+
+        return employees;
     }
 
     public static Employee buscarEmployee(String id) throws ArquivoException, EmployeeNaoEncontradoException {
-        return null;
+        Employee employee;
+        ObjectInputStream inputStream;
+
+        try {
+            inputStream = getInputStream(repositorioPath + id + ".byte");
+            employee = (Employee) inputStream.readObject();
+
+            inputStream.close();
+        } catch (IOException | ClassNotFoundException e) {
+            if (e.getClass().getSimpleName().equals("FileNotFoundException")) {
+                throw new EmployeeNaoEncontradoException("Employee não encontrado");
+            }
+
+            throw new ArquivoException("");
+        }
+
+        return employee;
     }
 
     public static void apagarEmployee(String id) throws ArquivoException, EmployeeNaoEncontradoException {
+        buscarEmployee(id);
+
+        new File(String.format("%s%s.byte", repositorioPath, id)).delete();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Employee employee = (Employee) obj;
+
+        return getId().equals(employee.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * 7 + ((id == null) ?  0 : id.hashCode());
     }
 
     public String getNome() {
-        return null;
+        return nome;
     }
 
     public void setNome(String nome) throws ComprimentoInvalidoException {
+        if (nome.length() < 3) {
+            throw new ComprimentoInvalidoException("Comprimento do nome deve ser maior que 2 caracteres.");
+        }
+
+        this.nome = nome;
     }
 
     public String getSobrenome() {
-        return null;
+        return sobrenome;
     }
 
     public void setSobrenome(String sobrenome) throws ComprimentoInvalidoException {
+        if (sobrenome.length() < 3) {
+            throw new ComprimentoInvalidoException("Comprimento do sobrenome deve ser maior que 2 caracteres.");
+        }
+
+        this.sobrenome = sobrenome;
     }
 
     public String getEmail() {
-        return null;
+        return email;
     }
 
     public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getId() {
         return id;
     }
-
 }
